a03bcf4b8bdbaebec924c19cc45ea6d0ab75f412
Michael van Tellingen <m.vantellingen@lukkien.com>
2015-05-12 09:02:47 +0200

    Update to 0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ tests_require = [
 
 setup(
     name='django-healthchecks',
-    version='0.1.0',
+    version='0.1.1',
     description=description,
     url='https://github.com/mvantellingen/django-healthchecks',
     author="Michael van Tellingen",
