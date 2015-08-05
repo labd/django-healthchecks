64471d39a9f8d8b0300c505966a7ee764c516bd8
Michael van Tellingen <michael@mvantellingen.nl>
2015-08-05 12:57:03 +0200

    Set proper caching headers (no-cache, no-store, max-age=0)

--- src/django_healthchecks/views.py
+++ src/django_healthchecks/views.py
@@ -1,17 +1,28 @@
 from django.http import JsonResponse, HttpResponse
 from django.http.response import Http404
+from django.views.decorators.cache import cache_control
 from django.views.generic import View
 
 from django_healthchecks.checker import create_report, create_service_result
 
 
-class HealthCheckView(View):
+class NoCacheMixin(object):
+
+    @classmethod
+    def as_view(cls, **kwargs):
+        view = super(NoCacheMixin, cls).as_view(**kwargs)
+        return cache_control(
+            private=True, no_cache=True, no_store=True, max_age=0)(view)
+
+
+class HealthCheckView(NoCacheMixin, View):
+
     def get(self, request, *args, **kwargs):
         report = create_report()
         return JsonResponse(report)
 
 
-class HealthCheckServiceView(View):
+class HealthCheckServiceView(NoCacheMixin, View):
     def get(self, request, service, *args, **kwargs):
         result = create_service_result(service)
         if result is None:
