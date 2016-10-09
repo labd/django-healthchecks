6260411f4b780b61416a0c121070e9a682d77e96
Michael van Tellingen <michael@mvantellingen.nl>
2016-10-09 22:10:09 +0200

    Bump to 1.1.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ tests_require = [
 
 setup(
     name='django-healthchecks',
-    version='1.0.0',
+    version='1.1.0',
     description="Simple Django app/framework to publish health checks",
     long_description=open('README.rst', 'r').read(),
     url='https://github.com/mvantellingen/django-healthchecks',
