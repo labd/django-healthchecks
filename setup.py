933af643b2f1e8c4d73ccf81cc3239ded295a5f6
Michael van Tellingen <michael@mvantellingen.nl>
2018-03-08 17:41:44 +0100

    Bump version: 1.4.1 → 1.4.2

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 docs_require = [
-    'sphinx>=1.4.1',
+    'sphinx>=1.4.0',
 ]
 
 tests_require = [
-    'coverage==.4.2',
+    'coverage==4.5.1',
     'pytest==3.0.5',
     'pytest-django==3.1.2',
     'requests-mock==1.4.0',
@@ -14,12 +14,12 @@ tests_require = [
     'isort==4.2.5',
     'flake8==3.0.3',
     'flake8-blind-except==0.1.1',
-    'flake8-debugger==1.4.1',
+    'flake8-debugger==1.4.0',
 ]
 
 setup(
     name='django-healthchecks',
-    version='1.4.1',
+    version='1.4.2',
     description="Simple Django app/framework to publish health checks",
     long_description=open('README.rst', 'r').read(),
     url='https://github.com/mvantellingen/django-healthchecks',
