fc3ce2847ed549f623d8aed17ec6d09b7454abe2
Michael van Tellingen <michael@mvantellingen.nl>
2018-03-08 17:41:40 +0100

    Revert bumpversion error

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ tests_require = [
     'coverage==.4.2',
     'pytest==3.0.5',
     'pytest-django==3.1.2',
-    'requests-mock==1.4.1',
+    'requests-mock==1.4.0',
 
     # Linting
     'isort==4.2.5',
