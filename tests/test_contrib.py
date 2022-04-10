a3d85ceb0f4dce739a3837de819d128844ba32a8
Storm Heg <storm@stormheg.co>
2022-04-10 17:16:40 +0200

    Fix contrib cache down test

--- tests/test_contrib.py
+++ tests/test_contrib.py
@@ -1,33 +1,41 @@
 import pytest
-from django.core import cache
 
-from django_healthchecks import contrib
+
+@pytest.fixture
+def contrib():
+    # The django_healthchecks contrib module imports `django.core.cache`. The cache will be configured using the settings
+    # at import time which makes it difficult to override the cache backend in individual tests.
+    # As a workaround, we import the contrib module here and use it as a fixture so the cache will only be configured
+    # when we use the contrib module in our tests.
+    from django_healthchecks import contrib
+
+    return contrib
 
 
 @pytest.mark.django_db
-def test_check_database():
+def test_check_database(contrib):
     assert contrib.check_database()
 
 
-def test_check_cache_default():
-    cache.caches = cache.CacheHandler()
+def test_check_cache_default(contrib):
     assert contrib.check_cache_default()
 
 
-def test_check_cache_default_down(settings):
+def test_check_cache_default_down(settings, contrib):
     """Check if the application can connect to the default cached and
     read/write some dummy data.
 
     """
-    cache.caches = cache.CacheHandler()
     settings.CACHES = {
-        "default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache",}
+        "default": {
+            "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+        }
     }
     assert not contrib.check_cache_default()
 
 
 @pytest.mark.django_db
-def test_check_heartbeats():
+def test_check_heartbeats(contrib):
     assert not contrib.MIGRATION_CACHE
     assert contrib.check_open_migrations()
     assert contrib.MIGRATION_CACHE
